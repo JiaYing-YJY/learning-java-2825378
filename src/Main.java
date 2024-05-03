0b18e7192a6ea9e16b424eb030df9d184fd5d247
JiaYing-YJY <84889593+JiaYing-YJY@users.noreply.github.com>
2024-05-03 06:53:14 +0000

    update

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,16 @@ public class Main {
         char studentFirstInitial = 'K';
         char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
+        String studentName = "Kayla Hammon";
+        String studentFirstName = "Kayla";
+        String studentLastName = "Hammon";
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
     }
 }
